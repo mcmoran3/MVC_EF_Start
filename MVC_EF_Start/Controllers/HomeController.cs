3fa50ec3928b9e90ca704a5a0e182b050a817e21
mcmoran3 <mcmoran3@usf.edu>
2021-12-02 15:05:56 -0500

    getting comittee info and displaying in API

--- MVC_EF_Start/Controllers/HomeController.cs
+++ MVC_EF_Start/Controllers/HomeController.cs
@@ -13,7 +13,8 @@ namespace MVC_EF_Start.Controllers
 
         static string BASE_URL = "https://api.open.fec.gov/v1/candidates/?sort_hide_null=false&page=10&per_page=20&api_key=";
 
-    //    static string BASE_URLCommittee = "https://api.open.fec.gov/v1/committees/?sort_hide_null=false&page=1&per_page=20&api_key=";
+        static string BASE_URLCommittee = "https://api.open.fec.gov/v1/committees/?sort_hide_null=false&page=1&per_page=20&api_key=";
+        static string BASE_URLInd = "https://api.open.fec.gov/v1/schedules/schedule_a/?per_page=20&sort_null_only=false&sort=-contribution_receipt_date&api_key=";
         static string API_KEY = "fZn1U5g0yNRjFfXNiDUs6oX0ciYldCwYSffwpJVD"; //Add your API key here inside ""
 
         //static string BASE_URL = "https://data.cdc.gov/api/views/hk9y-quqm/rows.json";
@@ -128,7 +129,49 @@ namespace MVC_EF_Start.Controllers
         public IActionResult committeetocandidate()
         {
 
-            return View();
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Add("X-Api-Key", API_KEY);
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            string NATIONAL_PARK_API_PATH = BASE_URLCommittee; // + "/parks?limit=20";
+            string parksData = "";
+
+            Parks parks = null;
+
+            httpClient.BaseAddress = new Uri(NATIONAL_PARK_API_PATH);
+
+            try
+            {
+                HttpResponseMessage response = httpClient.GetAsync(NATIONAL_PARK_API_PATH)
+                                                        .GetAwaiter().GetResult();
+                //HttpResponseMessage response = httpClient.GetAsync(BASE_URL)
+                //                                        .GetAwaiter().GetResult();
+
+
+
+                if (response.IsSuccessStatusCode)
+                {
+                    parksData = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                }
+
+                if (!parksData.Equals(""))
+                {
+                    // JsonConvert is part of the NewtonSoft.Json Nuget package
+                    parks = JsonConvert.DeserializeObject<Parks>(parksData);
+                }
+
+                //  dbContext.Parks.Add(parks);
+                // await dbContext.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                // This is a useful place to insert a breakpoint and observe the error message
+                Console.WriteLine(e.Message);
+            }
+
+            return View(parks);
         }
 
         public IActionResult contributionbyind()
